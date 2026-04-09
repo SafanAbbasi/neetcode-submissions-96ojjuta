fb30b385bdb86f4f1798fdba0f0f3681ef30d237
Safan Abbasi <safan.a.abbasi@gmail.com>
2026-04-09 04:32:43 -0500

    Add: trapping-rain-water - submission-1

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-1.py
@@ -0,0 +1,28 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        # to trap you need the minumum of the heights of left and right 
+        # increment the smaller height
+        # need to figure out when to add to total water
+        # trapped is min(hleft,hright) - height[i] where i would have to be less than that to trap water
+
+
+
+        left, right = 0, len(height) -1
+        leftMax = height[left]
+        rightMax = height[right]
+
+        total = 0
+
+        while left < right:
+
+            leftMax = max(leftMax, height[left])
+            rightMax = max(rightMax, height[right])
+
+            if leftMax < rightMax: 
+                total += ( leftMax - height[left] ) 
+                left += 1
+            else:
+                total += ( rightMax - height[right] )
+                right -= 1
+            
+        return total
