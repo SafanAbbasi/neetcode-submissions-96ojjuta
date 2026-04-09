5fea80ebf59fe8677326b5ea8df16bc8530d42b1
sabbasi3 <13663848+sabbasi3@users.noreply.github.com>
2026-04-09 03:05:37 -0500

    Add: two-integer-sum-ii - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        # sorted array
+        # example [ 1, 4, 7, 10, 12] # target = 14 answer is [2,4]
+        # shift pointer based off being lower or higher than target
+
+        left, right = 0 , len(numbers) - 1
+
+        while left < right:
+            total = numbers[left] + numbers[right] 
+
+            if total < target: # too small so left needs to increse
+                left += 1
+            
+            elif total > target: # too big
+                right -= 1
+            
+            if total == target:
+                return [left+1, right+1]
+        
