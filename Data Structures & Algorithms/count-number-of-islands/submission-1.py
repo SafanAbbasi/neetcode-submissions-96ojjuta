cbe079b1db520eeafa67dd0369107e08b100277d
sabbasi3 <13663848+sabbasi3@users.noreply.github.com>
2026-04-18 19:49:15 -0500

    Add: count-number-of-islands - submission-1

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-1.py
@@ -0,0 +1,61 @@
+from collections import deque
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        # rows, cols = len(grid), len(grid[0])
+        # visited = set()
+        # directions = [(1,0), (-1,0), (0,1), (0,-1)]
+
+
+#         def bfs(r, c):
+#             queue = deque()
+#             queue.append((r, c))
+#             visited.add((r, c))
+#             while queue:
+#                 cr, cc = queue.popleft()
+
+#                 for dr, dc in directions: # delta row and delta columns
+#                     nr, nc = cr + dr, cc + dc 
+#                     if 0 <= nr < rows and 0 <= nc < cols: # check if new row and col are in bounds
+#                         if (nr,nc) not in visited and grid[nr][nc] == '1':
+#                             visited.add((nr, nc))
+#                             queue.append((nr,nc))
+
+
+
+# #        outer logic that calls bfs(r, c) goes here. flood all connected island parts when you find a 1
+#         count = 0
+#         for r in range(rows):
+#             for c in range(cols):
+#                 if grid[r][c] == '1' and (r,c) not in visited:
+#                     count += 1
+#                     bfs(r, c)    # marks entire island as visited
+
+#         return count
+
+        rows, cols = len(grid), len(grid[0])
+        visited = set()
+        directions = [(1,0), (-1,0), (0,1), (0,-1)]
+
+        def dfs(r,c):
+
+            if r < 0 or r >= rows or c < 0  or c >= cols or grid[r][c] == "0" or (r, c) in visited:
+                return 
+            
+            visited.add((r,c))
+            
+            for dr, dc in directions:
+                
+                dfs(r + dr, c + dc)
+            
+
+#        outer logic that calls bfs(r, c) goes here. flood all connected island parts when you find a 1
+        count = 0
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == '1' and (r,c) not in visited:
+                    count += 1
+                    dfs(r, c)    # marks entire island as visited
+
+        return count
+
+            
