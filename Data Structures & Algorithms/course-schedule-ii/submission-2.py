7c3bcfe43b460e127e90df9df1170dac7cd837a9
sabbasi3 <13663848+sabbasi3@users.noreply.github.com>
2026-04-21 00:54:45 -0500

    Add: course-schedule-ii - submission-2

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-2.py
@@ -0,0 +1,42 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        
+
+        res = []
+        adj = defaultdict(list)
+        dep_count = [0] * (numCourses)
+        for a, b in prerequisites:
+
+            adj[b].append(a) # list contains everything that depends on b
+            dep_count[a] += 1
+
+        
+        q = collections.deque()
+
+        for course, dep in enumerate(dep_count):
+            if dep == 0:
+                q.append(course)
+
+        count = 0
+
+        # if q:
+        #     res.extend(q)
+        # else: 
+        #     return []
+        
+        while q:
+
+            nodep_course = q.popleft()
+            count += 1
+            res.append(nodep_course)
+
+            for course in adj[nodep_course]:
+                dep_count[course] -= 1
+
+                if dep_count[course] == 0:
+                    # res.append(course)
+                    q.append(course)
+            
+        return res if count == numCourses else []
+        
+        
